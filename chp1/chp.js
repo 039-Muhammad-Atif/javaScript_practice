ac6b5ca1cffe5bf5104404e4d6e1dca6990e8199
Muhammad Atif <atifwazir039@gmail.com>
2023-07-14 14:43:04 +0500

    We have run all the code for chp#1 Question

---
+++ chp1/chp.js
@@ -0,0 +1,30 @@
+// Answer of Q#1
+let a = "Atif"
+let b = 39
+console.log(a+b)
+
+// Answer of Q#2
+console.log(typeof(a+b))
+
+// answer of Q#3
+const obj = {
+    name: "Atif",
+    age: 23,
+    Occopation: "Developer"
+}
+//obj = 423
+
+//Answer of Q#4
+obj['Nationality'] = "Pakistan"
+
+console.log(obj)
+
+// Answer of Q#5
+const dict={
+    Invader: "Attacker to capture a tarrotry",
+    solely: "only or any things else",
+    vigorous: "Strong or full of energy",
+    mitigate: "reduced or weaken or lessen or lighten",
+    abolish: "fromaly put an End"
+}
+console.log(dict.vigorous)
