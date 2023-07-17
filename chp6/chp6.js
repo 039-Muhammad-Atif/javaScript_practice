e95d6b2c21c69121333d4233f056c4e50abad553
Muhammad Atif <atifwazir039@gmail.com>
2023-07-17 23:36:02 +0500

    Code for Chp#6 Questions

--- chp6/chp6.js
+++ chp6/chp6.js
@@ -1,1 +1,43 @@
-console.log("it's from console")
+// Answer to Question #1
+
+/* let age = prompt("Enter your age:");
+age = Number.parseInt(age)
+alert(age >= 18? "You Can Drive" : "You Can't Drive"); */
+
+//Answer to Question #2
+
+/* let runAgain = true;
+while(runAgain)
+{
+    let age = prompt("Enter your age:");
+    age = Number.parseInt(age)
+    alert(age >= 18? "You Can Drive" : "You Can't Drive");
+    runAgain = confirm("Do you want to play Again")
+} */
+
+// Answer to Question #3
+
+let runAgain = true;
+while(runAgain)
+{
+    let age = prompt("Enter your age:");
+    age = Number.parseInt(age)
+    if(age<=0)
+    {
+        console.error("Age must be greater than 0")
+    }
+    alert(age >= 18? "You Can Drive" : "You Can't Drive");
+    runAgain = confirm("Do you want to play Again")
+}
+
+// Answer to Question #4
+let num = prompt("Enter the Number:")
+num = Number.parseInt(num)
+if(num>4)
+{
+    location.href = "https://www.google.com"
+}
+
+// Answer to Question #5
+let color = prompt("Enter the background Color:")
+document.body.style.background = color;
