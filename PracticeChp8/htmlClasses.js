6e27464cf1736a2fe5e84d131372b8fdcb7f50e8
Muhammad Atif <atifwazir039@gmail.com>
2023-07-19 22:36:18 +0500

    Changing HTML Classes using JavaScript

---
+++ PracticeChp8/htmlClasses.js
@@ -0,0 +1,10 @@
+//Practice of ClassName and ClassList
+
+let first = document.getElementById("first");
+first.className = "wow yellow"
+first.classList.remove("wow");
+first.classList.add("text-dark");
+
+first.classList.contains("WoW");// It's Return True if the class is present else return false
+
+ 
