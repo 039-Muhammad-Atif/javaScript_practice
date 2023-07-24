edc264d52212a5092f6dbb123558ea01211bf099
Muhammad Atif <atifwazir039@gmail.com>
2023-07-24 16:25:51 +0500

    We can Solved Exercise by another way

--- exercise/exe5/exe5.js
+++ exercise/exe5/exe5.js
@@ -1,3 +1,4 @@
+/* 
 let hackfun = async()=>{
     let p1 = new Promise((resolve,reject) =>{
         setTimeout(() => {
@@ -42,4 +43,49 @@ let hackfun = async()=>{
     
 }
 
-hackfun()
+hackfun() */
+
+// ////////////////////// Solved By Harry Bhai \\\\\\\\\\\\\\\\\\\\\
+
+let message = [
+    "Initiating hack programe . . . . . ",
+    "Connecting to Server 1 . . . . . ",
+    "connection failed . . . . .",
+    "Connecting to Server 2 . . . . . ",
+    "connected successfuly . . . . .",
+    "Connecting to facebook . . . . . ",
+    "found User name . . . . .  ",
+    "Haching Abdullah user name . . . . .",
+    "User name found abdullah007 . . . . . ",
+    "200k password tried on  . . . . . ",
+    "password not match so for  . . . . . ",
+    "Again 200k password tried on  . . . . . ",
+    "password match successfully  . . . . . ",
+    "NOW Facebook Account Haccked"
+]
+let container = document.getElementById("container")
+
+const sleep = async (seconds)=>{
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(true)
+        }, seconds * 1000);
+    })
+}
+
+const showHack = async (message)=>{
+    await sleep(2);
+    console.log(message);
+    container.innerHTML = container.innerHTML + `<div id="promise1">${message} </div>`+"<br>"
+    
+    
+}
+
+const run = async ()=>{
+    for (let i = 0; i < message.length; i++) {
+        await showHack(message[i]);
+        
+    }
+}
+
+run()
